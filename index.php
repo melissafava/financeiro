68fa1cc8e3b32086bfacf0a8c523607151764494
Melissa <melissa.fava@aluno.senai.br>
2025-12-03 12:05:07 -0300

    commitei

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Financeiro</title>
+</head>
+<body>
+    <h1>Sistema Financeiro</h1>
+</body>
+</html>
