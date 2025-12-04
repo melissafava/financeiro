4bb59200d8bbb5485c183ddcc5122f2fb7ec5de4
Melissa <melissa.fava@aluno.senai.br>
2025-12-04 09:35:08 -0300

    autenticar login

--- index.php
+++ index.php
@@ -2,20 +2,30 @@
 require_once 'config.php';
 
 // verificar se o usuario esta logado
-if (!isset($_SESSION['usuario_id'])){
+if (!isset($_SESSION['usuario_id'])) {
     header('Location: login.php');
     exit;
 }
+
+$usuario_id = $_SESSION['usuario_id'];
+$usuario_nome = $_SESSION['usuario_nome'];
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema Financeiro</title>
 </head>
+
 <body>
     <h1>Sistema Financeiro</h1>
+
+    <div>
+        <p>Bem-vindo. <strong> <?php echo $usuario_nome ?> </strong></p>
+    </div>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -10,11 +10,13 @@ if (isset($_SESSION['usuario_id'])) {
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Sistema Financeiro</title>
 </head>
+
 <body>
     <h1>Login - Sistema Financeiro</h1>
     <form action="autenticar.php" method="post">
@@ -31,4 +33,5 @@ if (isset($_SESSION['usuario_id'])) {
 
     <p>Não tem conta? <a href="registro.php">Cadastre-se aqui.</a></p>
 </body>
+
 </html>
